f34d856cfcc779fc412c2540ec5168c5d2e78d69
Kwame Agyemang <agyemangyeb@gmailcom>
2021-09-13 01:44:10 -0400

    commit in progress

---
+++ src/components/Forms.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Forms extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Enter a Todo..." className="task-input"/>
+                <button className="button-add" type="submit">
+                    Add
+                </button>
+
+            </form>
+           
+        )
+    }
+}
