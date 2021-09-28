3942b5d661d5727f39b7d57d5b7866f26a6545b8
Kwame Agyemang <agyemangyeb@gmailcom>
2021-09-28 00:24:00 -0400

    commit complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import TodoList from './components/TodoList';
-import Header from './components/Header';
-import './App.css';
-import Quote from './components/Quote';
-
+import TodoList from "./components/TodoList";
+import { Switch, Route } from 'react-router-dom'
+import Header from "./components/Header";
+import "./App.css";
+import Quote from "./components/Quote";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 const App = () => {
-
-
   return (
-     <div className='app'>
-       <div>
-       <Header />
-       <TodoList />
-       </div>
-       <Quote /> 
-     </div>
-   );
- }
+    <div className="app">
+      <Navbar />
 
- export default App;
+      <Switch>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <div>
+        <Header />
+        <TodoList />
+      </div>
+      <Quote />
+    </div>
+  );
+};
+
+export default App;
